c9c471ff67997f00327c4880f4f0e838212fe32e
Ben Spark <bhrugen1990@gmail.com>
2017-10-01 18:25:52 -0500

    Initial commit

---
+++ QuirkyBookRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuirkyBookRental.Startup))]
+namespace QuirkyBookRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
