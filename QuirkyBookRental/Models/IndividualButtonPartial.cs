be245505783851283be0f1e06139bc3b4083c420
Ben Spark <bhrugen1990@gmail.com>
2017-10-13 11:51:20 -0500

    Section 2 - 5 Table Button Partial View

---
+++ QuirkyBookRental/Models/IndividualButtonPartial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace QuirkyBookRental.Models
+{
+    public class IndividualButtonPartial
+    {
+        public string ButtonType { get; set; }
+        public string Action { get; set; }
+        public string Glyph { get; set; }
+        public string Text { get; set; }
+
+        public int? GenreId { get; set; }
+        public int? BookId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? MembershipTypeId { get; set; }
+
+        public string ActionParameter
+        {
+            get
+            {
+                var param = new StringBuilder(@"/");
+
+                if(BookId !=null && BookId > 0)
+                {
+                    param.Append(String.Format("{0}", BookId));
+                }
+                if (GenreId != null && GenreId> 0)
+                {
+                    param.Append(String.Format("{0}", GenreId));
+                }
+                if (MembershipTypeId != null && MembershipTypeId > 0)
+                {
+                    param.Append(String.Format("{0}", MembershipTypeId));
+                }
+                if (CustomerId != null && CustomerId > 0)
+                {
+                    param.Append(String.Format("{0}", CustomerId));
+                }
+
+                return param.ToString();
+            }
+        }
+    }
+}
