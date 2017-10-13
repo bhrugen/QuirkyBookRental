bd2a4cfa51d334d6bdebb881d313d3512d36423f
Ben Spark <bhrugen1990@gmail.com>
2017-10-13 10:45:10 -0500

    Section 2 - 3 Create Genre Controller and View

---
+++ QuirkyBookRental/Controllers/GenreController.cs
@@ -0,0 +1,36 @@
+﻿using QuirkyBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuirkyBookRental.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
