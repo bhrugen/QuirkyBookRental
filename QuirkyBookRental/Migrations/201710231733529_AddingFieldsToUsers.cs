73ff8d676ab3c53d0028171f90e7f50e1fab71ad
Ben Spark <bhrugen1990@gmail.com>
2017-10-23 12:36:01 -0500

    Section 4 - Adding fields to Users Table

---
+++ QuirkyBookRental/Migrations/201710231733529_AddingFieldsToUsers.cs
@@ -0,0 +1,28 @@
+namespace QuirkyBookRental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingFieldsToUsers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "Phone", c => c.String());
+            AddColumn("dbo.AspNetUsers", "BirthDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.AspNetUsers", "Disable", c => c.Boolean(nullable: false));
+            AddColumn("dbo.AspNetUsers", "MembershipTypeId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "MembershipTypeId");
+            DropColumn("dbo.AspNetUsers", "Disable");
+            DropColumn("dbo.AspNetUsers", "BirthDate");
+            DropColumn("dbo.AspNetUsers", "Phone");
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+        }
+    }
+}
--- QuirkyBookRental/Models/IdentityModels.cs
+++ QuirkyBookRental/Models/IdentityModels.cs
@@ -3,12 +3,20 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace QuirkyBookRental.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Disable { get; set; }
+        public int MembershipTypeId { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
