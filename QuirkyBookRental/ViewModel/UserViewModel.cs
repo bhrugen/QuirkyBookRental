922e00747042f5f068f9f011dc49d6b206d216bc
Ben Spark <bhrugen1990@gmail.com>
2017-10-25 20:00:32 -0500

    Section 5 - Create User View Model

---
+++ QuirkyBookRental/ViewModel/UserViewModel.cs
@@ -0,0 +1,47 @@
+﻿using QuirkyBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuirkyBookRental.ViewModel
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public ICollection<MembershipType> MembershipTypes { get; set; }
+
+        [Required]
+        public int MembershipTypeId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:MM dd yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        public bool Disabled { get; set; }
+
+
+    }
+}
